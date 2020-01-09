027ce64fc167e48da0baa5a3d19a3775798f5e2b
Welton Leao Machado <wlmachado@bbmapfre.com.br>
2020-01-09 11:29:51 -0300

    aaaaaaaaaaaa

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const compression = require('compression');
 const bodyparser = require('body-parser');
+const https = require('https');
 
 const app = express();
 app.use(compression());
@@ -9,6 +10,24 @@ app.use(bodyparser.json());
 
 app.get('/', function(req, res) {
     res.json({mensagem: "ok"});
+});
+
+
+app.get('/getFilmsByPlanetName', (req, res) => {
+    const planetsearch_url = `https://swapi.co/api/planets`;
+    https
+      .get(planetsearch_url, res => {
+        let data = "";
+        res.on("data", d => {
+          data += d;
+        });
+        res.on("end", () => {
+          res.send('deu certo');
+        });
+      })
+      .on("error", e => {
+        res.send('deu errado');
+      });
 });
 
 app.listen(process.env.PORT || 4000, function(){
