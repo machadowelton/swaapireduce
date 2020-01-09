b58bacd0d44bd167c01ecf4aca3d6a85b52dab31
Welton Leao Machado <wlmachado@bbmapfre.com.br>
2020-01-09 11:26:26 -0300

    correção

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const compression = require('compression');
 const bodyparser = require('body-parser');
 
+const app = express();
 app.use(compression());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
